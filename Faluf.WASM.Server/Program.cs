647255175dd738409c8a366bb2fe77b9844352e3
Danny Møller <dannymoller@live.dk>
2023-04-29 20:11:42 +0200

    swagger

--- Faluf.WASM.Server/Program.cs
+++ Faluf.WASM.Server/Program.cs
@@ -3,6 +3,9 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -13,6 +16,9 @@ else
 {
 	app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(options => options.DefaultModelsExpandDepth(-1));
 
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
