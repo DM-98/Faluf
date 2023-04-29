fc18af1f2c2fa22b4c50956302d00264b5c39481
Danny Møller <dannymoller@live.dk>
2023-04-29 19:46:59 +0200

    controller test

---
+++ Faluf.WASM.Server/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Faluf.WASM.Server.Controllers;
+
+[Route("auth")]
+[ApiController]
+public sealed class AuthController : ControllerBase
+{
+	[HttpGet("Test")]
+	public ActionResult GetTest()
+	{
+		return Ok("Hello World");
+	}
+}
