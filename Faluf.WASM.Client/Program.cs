ae1abc22d7a94cb9667fa2ce1a218c95bc59dfe1
Danny Møller <dannymoller@live.dk>
2023-04-30 03:05:41 +0200

    removed rewrite package, using config.json

--- Faluf.WASM.Client/Program.cs
+++ Faluf.WASM.Client/Program.cs
@@ -1,7 +1,6 @@
 using Faluf.WASM.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Rewrite;
 using MudBlazor.Services;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -12,6 +11,5 @@ builder.Services.AddMudServices();
 builder.Services.AddHttpClient("Faluf.WASM.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Faluf.WASM.ServerAPI"));
-builder.Services.Configure<RewriteOptions>(options => options.AddRedirectToWwwPermanent());
 
 await builder.Build().RunAsync();
