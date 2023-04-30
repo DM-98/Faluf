89156f62d78e571307b7ee63f6947a01612ff81b
Danny Møller <dannymoller@live.dk>
2023-04-30 02:54:32 +0200

    redirect to www

--- Faluf.WASM.Client/Program.cs
+++ Faluf.WASM.Client/Program.cs
@@ -1,6 +1,7 @@
 using Faluf.WASM.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using MudBlazor.Services;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,7 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddHttpClient("Faluf.WASM.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
-// Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Faluf.WASM.ServerAPI"));
+builder.Services.Configure<RewriteOptions>(options => options.AddRedirectToWwwPermanent());
 
 await builder.Build().RunAsync();
