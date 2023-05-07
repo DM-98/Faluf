bab983a8439606934009bcde157239589a957104
Danny Møller <dannymoller@live.dk>
2023-05-07 14:26:56 +0200

    Bulk Data example (MudDataGrid). Fjern "| Home".

--- Faluf.WASM.Client/Program.cs
+++ Faluf.WASM.Client/Program.cs
@@ -4,12 +4,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-builder.Services.AddHttpClient("Faluf.WASM.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Faluf.WASM.ServerAPI"));
 
 await builder.Build().RunAsync();
